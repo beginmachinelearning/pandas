216384f1cfc5fc140d0c80f5cc7b8148fe4a1c37
maximdsouzaml <36703574+maximdsouzaml@users.noreply.github.com>
2018-04-04 19:15:05 +0530

    panda

--- 4_dataframe_arithmetic.py
+++ 4_dataframe_arithmetic.py
@@ -1,7 +1,28 @@
 import pandas as pd
 import numpy as np
 
-river_dict = {'river': ['Missouri', 'Mississippi', 'Yukon', 'Rio Grande', 'Arkansas'],
-        'length': [2341, 2202, 1979, 1759, 1450],
-        'state': ['Montana', 'Minnesota', 'British Columbia', 'Colorado', 'Kansas']}
-river_frame =pd.DataFrame(river_dict, columns=['river', 'length', 'state'])
+my_df=pd.DataFrame(np.arange(10))
+
+#Add 1
+my_df.add(1)
+
+#Sub 1
+my_df.sub(1)
+
+#reverse sub
+my_df.rsub(1)
+
+#Divide
+my_df.div(2)
+
+#Reverse Divide
+my_df.rdiv(1)
+
+#Multiply
+my_df.mul(10)
+
+#Exponent
+my_df.pow(2)
+
+#Using NumPy Abs
+np.abs(my_df.sub(10))
