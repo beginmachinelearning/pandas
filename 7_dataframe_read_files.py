43f9cd691757931590f66110396ad24db2f0dc9a
Maxim Dsouza <maximdsouza87@gmail.com>
2018-04-05 13:45:15 +0530

    pandas

--- 7_dataframe_read_files.py
+++ 7_dataframe_read_files.py
@@ -8,3 +8,20 @@ df=pd.read_csv("datasets/Social_Network_Ads_no_header.csv")
 
 #Tell the Dataframe this csv has no header row
 df=pd.read_csv("datasets/Social_Network_Ads.csv", header=None)
+
+#Set one column as index column
+df=pd.read_csv("datasets/Social_Network_Ads.csv", index_col='User ID')
+
+
+#Read from text file using separator
+df=pd.read_csv("datasets/text_single_space.txt", sep=" ")
+
+
+#Does not work for irregular spaces
+df=pd.read_csv("datasets/text_data.txt", sep=" ")
+
+#Read from text file using separator
+df=pd.read_csv("datasets/text_data.txt", delim_whitespace=True)
+
+#Write to a file
+df.to_csv("datasets/write_csv.csv")
